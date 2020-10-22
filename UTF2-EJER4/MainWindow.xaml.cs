7da50a92bef528644807e6d1942c7e8de4281fe2
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-10-22 12:31:04 +0100

    Agregar archivos de proyecto.

---
+++ UTF2-EJER4/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace UTF2_EJER4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CaracteresTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CaracteresTextBox.AcceptsReturn = true;
+            CaracteresTextBox.MaxLength = 140;
+            if (CaracteresTextBox.Text.Length == 140)
+                CaracteresTextBox.IsEnabled = false;
+            contadorTextBlock.Text = CaracteresTextBox.Text.Length + " /  140";
+        }
+    }
+}
